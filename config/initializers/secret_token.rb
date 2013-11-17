3d281e0e164491aa3eb0de847c69d8369693a087
prabhakar <undurthi.prabhakar@gmail.com>
2013-11-17 12:54:17 +0530

    Generated the  rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hireme::Application.config.secret_token = '2b01add6228a0f3e4d7c890ba18618c482551f126dc485ba512809bf4453c54aa821c9f884f270f19ba665eb4a786928e85f98ebdbf7df859182236e8b9821d7'
