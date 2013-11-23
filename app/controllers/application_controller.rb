79576a874ebd54e0eda70e3e5d2c9498aeb115d8
prabhakar <undurthi.prabhakar@gmail.com>
2013-11-23 12:08:02 +0530

    Completed code for user log out session.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,31 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+
+# these two helper methods often used in this application .
+  helper_methods :current_user_session, :current_user
+
+  private
+
+# If the currently logged in user session is defiend,
+# It returns that user session from the catche OR database.
+# Otherwise it finds that user session
+
+  def current_user_session
+  	return @current_user_session  if defined(@current_user_session)
+  	@current_user_session = UserSession.find
 end
+
+    # If the currently logged in user is defined already, It returns that user details.
+	# Otherwise,it checks the following :
+
+	# when the current user = current user session , then It would call the find method
+	# on that user.
+
+def current_user
+	return @current_user if defined?(@current_user)
+	@current_user = current_user_session && current_user_session.user
+	end
+end
+
+
--- app/controllers/user_session_controller.rb
+++ app/controllers/user_session_controller.rb
@@ -1,5 +1,8 @@
 class UserSessionController < ApplicationController
 
+	# When new user wants to register on the site
+	# It allocates new session.
+
 	def new
 		@user_session = UserSession.new
 end
@@ -8,10 +11,32 @@ end
 def create 
 	@user_session = UserSession.new(params[:user_session])
 
+	# The new user detials would be saved in the database, and It will falsh a message
+    # to the new user .i.e welcome message.
+
+    # If the user enters wrong information in the params[:user-session]
+    # then, the application will not able to save the details, Instead 
+    # It will  produce a new form again to fill the details.
+
+
 
 if @user_session.save
 	falsh[:notice]  = "Welcome to Hireme"
 	redirect_to jobs_url
-else
-end
+	else
+	  render :action => :new
+	  end
+
+
+
+# when the current user session is availble , then  the destroy
+# method would cause to to call the destroy on that user.
+# 
+  def destroy
+  	current_user_session && current_user.destroy
+  	falsh[:notice] = "You are now logged out !!!"
+  	redirect_to jobs_url
+
+   end
+
 end
